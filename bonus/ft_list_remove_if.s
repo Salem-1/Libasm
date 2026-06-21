4ec2d1ed5c5c5b19f2460a317de6bd84059d4b0e
Ahmed Salem <61345528+Salem-1@users.noreply.github.com>
2026-06-21 14:59:13 +0000

    did the mock finally with ugly logic

---
+++ bonus/ft_list_remove_if.s
@@ -0,0 +1,17 @@
+%include "headers.inc"
+
+;extern void 	ft_list_sort(t_list **begin_list, int (*cmp)());
+
+section .data
+    begin_list   dq 0x00
+    cmp_function dq 0x00
+
+section .text
+global ft_list_sort
+extern malloc
+
+ft_list_sort:
+    xor rax, rax
+    ret
+
+
